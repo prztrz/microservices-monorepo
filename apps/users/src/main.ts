b9a032a8831b8a7db68c108daeabc358796bf2b1
Przemek Trzepinski <ptrzepinski@gmail.com>
2025-05-14 18:25:37 +0200

    docker config

--- apps/users/src/main.ts
+++ apps/users/src/main.ts
@@ -2,11 +2,20 @@ import { NestFactory } from '@nestjs/core';
 import { UsersModule } from './users.module';
 import { config } from 'dotenv';
 import { join } from 'path';
+// import { ValidationPipe } from '@nestjs/common';
 
 config({ path: join(__dirname, '../../../.env') });
 
 async function bootstrap() {
   const app = await NestFactory.create(UsersModule);
+  // app.useGlobalPipes(
+  //   new ValidationPipe({
+  //     transform: true,
+  //     whitelist: true,
+  //     forbidNonWhitelisted: true,
+  //     forbidUnknownValues: true,
+  //   }),
+  // );
   await app.listen(process.env.USERS_PORT);
 }
 bootstrap();
